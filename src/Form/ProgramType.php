462df0106d5bb09f8d39f53a3c68a938f0092960
Marie-Camille <mariecamille.dorio@gmail.com>
2020-12-14 16:11:14 +0100

    quête numéro 15

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Entity\Actor;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 {
@@ -19,6 +21,13 @@ class ProgramType extends AbstractType
             ->add('country', TextType::class)
             ->add('year', TextType::class)
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false
+            ])
         ;
     }
 
